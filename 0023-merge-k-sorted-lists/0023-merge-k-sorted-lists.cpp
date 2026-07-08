d06c839a854e8469ab902401ba614df2e06b2a4f
Ritika Raghav <raghavritika.official@gmail.com>
2026-07-08 20:24:55 +0530

    Time: 0 ms (100%), Space: 18.7 MB (38.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,51 +1,36 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-
-
-
-// Custom comparator to create a min-heap based on node values
-struct CompareNode {
-    bool operator()(const ListNode* a, const ListNode* b) {
-        // Returns true if 'a' has higher value than 'b'
-        // This ensures the SMALLEST value is always at the top
-        return a->val > b->val;
-    }
-};
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
-        // Define the min-heap priority queue
-        std::priority_queue<ListNode*, std::vector<ListNode*>, CompareNode> minHeap;
-        
-        // Step 1: Push the head of each non-empty linked list into the heap
-        for (ListNode* head : lists) {
-            if (head != nullptr) {
-                minHeap.push(head);
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>> pq;
+
+        for(int i = 0; i < lists.size(); ++i) {
+            if(lists[i]) pq.push({-lists[i]->val, lists[i]});
+        }
+
+        auto head = new ListNode();
+        auto cur = head;
+
+        while(!pq.empty()) {
+            cur->next = pq.top().second;
+            pq.pop();
+            cur = cur->next;
+            if(cur->next) {
+                pq.push({-cur->next->val, cur->next});
             }
         }
-        
-        // Create a dummy node to easily build the result list
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
-        
-        // Step 2: Process the nodes until the heap is empty
-        while (!minHeap.empty()) {
-            // Get the node with the smallest value
-            ListNode* smallestNode = minHeap.top();
-            minHeap.pop();
-            
-            // Append it to our merged list
-            tail->next = smallestNode;
-            tail = tail->next;
-            
-            // If this node has a next node, push it into the heap
-            if (smallestNode->next != nullptr) {
-                minHeap.push(smallestNode->next);
-            }
-        }
-        
-        return dummy.next;
+
+        head = head->next;
+
+        return head;
     }
 };
