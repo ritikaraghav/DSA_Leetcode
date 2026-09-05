56a37ade1fb9a72cd15ca70b26ac5f1efbfe6d32
Ritika Raghav <raghavritika.official@gmail.com>
2026-09-05 21:26:00 +0530

    Time: 0 ms (100%), Space: 10 MB (52.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,18 @@
-#include <string>
-#include <algorithm>
-#include <cctype>
-
 class Solution {
 public:
-    bool isPalindrome(std::string s) {
-        s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char c) {
-            return !std::isalnum(c); 
-        }), s.end());
+    bool isPalindrome(string s) {
+        s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char x) {
+        return !std::isalnum(x);}), s.end());
 
-        // 2. NOW initialize your pointers based on the new size
         int left = 0;
-        int right = s.size() - 1;
+        int right = s.size()-1;
 
-        // 3. Compare using case-insensitive check
-        while (left < right) {
-            if (std::tolower(s[left]) != std::tolower(s[right])) {
+        while(left <= right){
+            if(tolower(s[left]) != tolower(s[right])){
                 return false;
             }
-            right--;
             left++;
+            right--;
         }
         return true;
     }
