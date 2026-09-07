19c737e2d9063c59c4686d8a57cc55a9be4482c1
Ritika Raghav <raghavritika.official@gmail.com>
2026-09-08 00:39:47 +0530

    Time: 379 ms (54.53%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,7 +2,8 @@
 Select 
     m.name
     from employee as e
-        inner join employee as m
-        on e.managerId=m.id
+    inner join employee as m
+    on 
+        e.managerId=m.id
     group by e.managerId 
     having count(e.id)>=5
