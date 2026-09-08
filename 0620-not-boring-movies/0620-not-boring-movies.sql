56a9e910824fd761e322c3f2251528a54be98c0b
Ritika Raghav <raghavritika.official@gmail.com>
2026-09-08 10:07:58 +0530

    Time: 1604 ms (5%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select * from Cinema where description <> "boring" and id%2 <> 0 order by rating desc;
+select 
+    * 
+    from Cinema 
+    where description <> "boring" and id%2 <> 0 
+    order by rating desc;
