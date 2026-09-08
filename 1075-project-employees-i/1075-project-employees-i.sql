bcf79f3d7ad04f57a503f61ae2307c81b05a50d1
Ritika Raghav <raghavritika.official@gmail.com>
2026-09-08 22:27:31 +0530

    Time: 570 ms (51.55%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -2,5 +2,6 @@
 select
     project_id, round(avg(e.experience_years),2) as average_years 
     from Project p
-    join Employee e on e.employee_id = p.employee_id
+    join Employee e 
+    on e.employee_id = p.employee_id
     group by project_id;
