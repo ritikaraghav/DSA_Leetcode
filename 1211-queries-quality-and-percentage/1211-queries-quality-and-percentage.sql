156802ab5cf6a70c5d3ea627e631c405daceb362
Ritika Raghav <raghavritika.official@gmail.com>
2026-09-08 22:50:57 +0530

    Time: 380 ms (68.72%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -9,5 +9,5 @@ SELECT
         (COUNT(CASE WHEN rating < 3 THEN 1 END) / COUNT(*)) * 100,
         2
     ) AS poor_query_percentage
-FROM queries 
-GROUP BY query_name;
+    FROM queries 
+    GROUP BY query_name;
